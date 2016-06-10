eae3dbc9a639dad362d81b139114eabca5dbd349
bigTeemo <jianlv281@gmail.com>
2016-06-09 23:06:35 -0400

    Finished part-3

--- Week-1/server.js
+++ Week-1/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var dishRouter = require('./dishRouter');	
 var promoRouter = require('./promoRouter');
+var leaderRouter = require('./leaderRouter');
 var morgan = require('morgan');
 var hostname = 'localhost';
 var port = 3000;
@@ -8,6 +9,7 @@ var app = express();	// Create an app
 app.use(morgan('dev'));	// Add log feature to the server
 app.use('/dishes',dishRouter); // If the URL contains the /dishes, then apply dishRouter
 app.use('/promotions', promoRouter);
+app.use('/leadership', leaderRouter);
 app.use(express.static(__dirname + '/public'));	// use express.static middleware 
 // and use the public folder under current folder as the one which contains the static files
 
